ed894fd6ca97f729f6a3cdd2e3e2df430dc8b2e7
luisHuizaBlanco <lgh7953@nyu.edu>
2024-04-18 15:15:19 -0400

    First milestone finished

--- nyufile.c
+++ nyufile.c
@@ -55,10 +55,10 @@ typedef struct DirEntry {
 void validate()
 {
     printf("Usage: ./nyufile disk <options>\n");
-    printf("-i                     Print the file system information.\n");
-    printf("-l                     List the root directory.\n");
-    printf("-r filename [-s sha1]  Recover a contiguous file.\n");
-    printf("-R filename -s sha1    Recover a possibly non-contiguous file.\n");
+    printf("  -i                     Print the file system information.\n");
+    printf("  -l                     List the root directory.\n");
+    printf("  -r filename [-s sha1]  Recover a contiguous file.\n");
+    printf("  -R filename -s sha1    Recover a possibly non-contiguous file.\n");
 
     exit(0);
 }
